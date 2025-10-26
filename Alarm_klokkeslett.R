1647e13c520bf15a6200a9e5c5ac354112001ddd
Eigenvalue <36112011+Postskjerm@users.noreply.github.com>
2025-10-26 13:17:49 +0100

    Initial commit

---
+++ Alarm_klokkeslett.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+library(beepr)
+
+alarm <- function(alarm_time,alarm_min){
+  nå_min <- format(Sys.time(),"%M") %>% as.numeric() %>%  print()
+  
+  nå_time <- format(Sys.time(),"%H") %>% as.numeric() %>%  print()
+  
+  tidsdifferanse = (alarm_time - nå_time) * 60 + alarm_min - nå_min 
+  
+  
+  while (tidsdifferanse > 2) {
+    Sys.sleep(60 * 1)
+    
+    nå_min <- format(Sys.time(),"%M") %>% as.numeric() %>%  print()
+    
+    nå_time <- format(Sys.time(),"%H") %>% as.numeric() %>%  print()
+    
+    tidsdifferanse = (alarm_time - nå_time) * 60 + alarm_min - nå_min 
+    print(tidsdifferanse)
+    
+  }
+  
+  beep(8)
+  
+}
+alarm(13,19)
+
+
+
+
+
